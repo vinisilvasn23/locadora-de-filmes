33ffcddd9a88e229308c8ae8814315ceb56ebdf7
Vinicius <vini456nascimento@gmail.com>
2023-08-19 10:02:42 -0300

    feat: update database

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ const client: Client = new Client({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
-  database: process.env.DB,
+  database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
 });
 
