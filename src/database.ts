b97e5b8aef69976cb5434e3227bb7f16e29cfe00
Vinicius <vini456nascimento@gmail.com>
2023-08-19 09:25:00 -0300

    feat: database preparation

---
+++ src/database.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { Client } from "pg";
+
+const client: Client = new Client({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  database: process.env.DB,
+  port: Number(process.env.DB_PORT),
+});
+
+const startDatabase = async (): Promise<void> => {
+  await client.connect();
+  console.log("Database connected.");
+};
+
+export { client, startDatabase };
